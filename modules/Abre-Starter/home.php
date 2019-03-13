6877a9a068785b83a0dabf1dcd3ad6fe5b2c153a
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-03-13 17:04:15 +0000

    first layout test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -32,12 +32,9 @@
         </div>
         <div class='row'>
             <div class='input-field col s10'>
-                <?php
-                echo "<meta name='theme-color' content='".$siteColor."'>";
-				?>
                 <input placeholder='Add New Task' id='new_task' type='text' class='validate'>
             </div>
-            <a class='btn-floating btn-large waves-effect waves-light'><i class='material-icons'>add</i></a>
+            <a class='btn-floating btn-large waves-effect waves-light' style='background-color: <?php echo $siteColor; ?>;'><i class='material-icons'>add</i></a>
         </div>
 		
 
