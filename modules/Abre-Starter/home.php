7c889fd3455d54f381871efe9541d252fce9df10
christianKahle <ckahle05@gmail.com>
2019-03-13 13:38:14 -0400

    First Layout Test - fixing button color

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -35,7 +35,7 @@
             <div class='input-field col s10'>
                 <input placeholder='Add New Task' id='new_task' type='text' class='validate'>
             </div>
-            <a href='elements.php'><a class='btn-floating btn-large waves-effect waves-light' style='theme-color: <?php echo $siteColor; ?>'><i class='material-icons'>add</i></a></a>
+		    <a class='btn-floating btn-large waves-effect waves-light' style='background-color:<?php echo $siteColor; ?>; left:20px;'><i class='material-icons'>add</i></a>
         </div>
 		
 
