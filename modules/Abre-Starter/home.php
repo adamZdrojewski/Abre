241a12ab21565a68eb5dfa39ba4a6013acd517ec
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-03-14 19:45:40 +0000

    array display test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -39,6 +39,26 @@
 	</div>
 </div>
 
+<?php $tasks = array("Math", "Science", "Computer Science");?>
+
+<?php if (count($tasks) > 0): ?>
+<table>
+  <thead>
+    <tr>
+      <th><?php echo implode('</th><th>', array_keys(current($shop))); ?></th>
+    </tr>
+  </thead>
+  <tbody>
+<?php foreach ($tasks as $row): array_map('htmlentities', $row); ?>
+    <tr>
+      <td><?php echo implode('</td><td>', $row); ?></td>
+    </tr>
+<?php endforeach; ?>
+  </tbody>
+</table>
+<?php endif; ?>
+
+
 <script>
 
 	$(function(){
