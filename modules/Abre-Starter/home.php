ca747fcc235889bd6a3a6bfb0e2ff790cd0f014a
christianKahle <ckahle05@gmail.com>
2019-03-14 13:42:14 -0400

    Testing form & array using php

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -39,9 +39,9 @@
                 <a class='btn-floating btn-large waves-effect waves-light' style='background-color:<?php echo $siteColor; ?>; left:20px;'><i class='material-icons'>add</i></a>
             </div>
         </div>
-        <?php ?>
+        <?php  ?>
         <b><?php foreach ($tasks as $task ) {
-            echo "\r\n" . $task;
+            echo "$task\r\n";
         }?></b>
 
 	</div>
