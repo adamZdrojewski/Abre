15073412427915cfe4c1e3fdc3ae577456348158
christianKahle <ckahle05@gmail.com>
2019-03-14 10:36:32 -0400

    Testing form & array using php

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -35,10 +35,10 @@
             <div class='input-field col s10' style='#FFFFFF; left:20px;'>
                 <form class="" id="subject" method="post" action="" >   
                     <input placeholder='Add New Task' id='new_task'  type='text' class='validate' name='tasks[]'>
-                    <!--<select name="tasks[]" multiple="yes">-->
+                    <select name="tasks[]" multiple="yes">
+                    <a class='btn-floating btn-large waves-effect waves-light' style='background-color:<?php echo $siteColor; ?>; left:20px;'><i class='material-icons'>add</i></a>
                 </form>
             </div>
-		    <a class='btn-floating btn-large waves-effect waves-light' style='background-color:<?php echo $siteColor; ?>; left:20px;'><i class='material-icons'>add</i></a>
         </div>
         <?php echo $_POST['tasks[]']?>
 
