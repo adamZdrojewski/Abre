926fa198648e1424b19cbf7e9c3d5b3c6b6e330a
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-03-14 19:22:38 +0000

    layout test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -30,10 +30,10 @@
                 <span style='font-size: 32px; font-weight:700'>Planner</span>
             </div>
         </div>
-        <div class='row'>
+        <div class="row">
             <div class='input-field col s10' style='#FFFFFF; left:20px;'>
                 <form class="" id="planner" method="post" action="" >   
-                    <input placeholder='Add New Task' id='new_task'  type='text' class='validate' name='newTask'>
+                    <input placeholder='Add New Task' id='new_task' type='text' class='validate' name='newTask'>
                     <button type="submit" class="modal-action waves-effect btn-floating btn-large waves-light s2" style='background-color: <?php echo $siteColor; ?>;'><i class='material-icons'>add</i></button>
                 </form>
             </div>
