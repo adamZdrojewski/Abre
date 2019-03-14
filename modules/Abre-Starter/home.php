b113a05377cfcfc92ca152963d013d2ba0bdec9e
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-03-14 13:06:47 +0000

    first arr test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -35,9 +35,22 @@
             <div class='input-field col s10'>
                 <input placeholder='Add New Task' id='new_task' type='text' class='validate'>
             </div>
-		    <a class='btn-floating btn-large waves-effect waves-light' style='background-color:<?php echo $siteColor; ?>; left:20px;'><i class='material-icons'>add</i></a>
+		    <a class='btn-floating btn-large waves-effect waves-light' onclick='addNew();' style='background-color:<?php echo $siteColor; ?>; left:20px;'><i class='material-icons'>add</i></a>
         </div>
+        
+        <script>
+            document.write(arr);
+        </script>
 		
 
 	</div>
 </div>
+
+<script>
+    var arr = ['Math', 'Science'];
+    
+    function addNew()
+    {
+        arr.push(new_task)
+    }
+</script>
