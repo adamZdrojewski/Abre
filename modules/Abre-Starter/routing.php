3bdae91f1c2f9bd82c71fca0d3b90a973ab7bfeb
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-03-19 23:48:37 +0000

    array test

--- modules/Abre-Starter/routing.php
+++ modules/Abre-Starter/routing.php
@@ -40,6 +40,16 @@
 						document.title = 'Starter';
 					$('#content_holder').load('modules/".basename(__DIR__)."/elements.php', function() { init_page(); });
 					$( '#modal_holder' ).load( 'modules/".basename(__DIR__)."/modals.php' );
-				},";
+				},
+                'starter': function()
+				{
+				    $('#navigation_top').hide();
+				    $('#content_holder').hide();
+				    $('#loader').show();
+				    $('#titletext').text('Starter');
+				    document.title = 'Starter';
+					$('#content_holder').load('modules/".basename(__DIR__)."/display.php', function() { init_page(); });
+					$( '#modal_holder' ).load( 'modules/".basename(__DIR__)."/modals.php' );
+			  },";
 		}
 ?>
