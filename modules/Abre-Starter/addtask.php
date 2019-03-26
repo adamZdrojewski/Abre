55a7882829e3ce412bb6c606719ccb745cab0f36
BuildTools <unconfigured@null.spigotmc.org>
2019-03-25 21:46:12 -0400

    array test

--- modules/Abre-Starter/addtask.php
+++ modules/Abre-Starter/addtask.php
@@ -22,17 +22,15 @@
     require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
 	session_start();
     $siteColor = getSiteColor();
-?>
-<?php
 
 echo "<h5>1</h5>";
-$tasks = $_SESSION['tasks'];
+/*$tasks = $_SESSION['tasks'];
 $task = $_POST['task'];
 echo "<h5>2</h5>";
 array_push($tasks , $task);
 echo "<h5>3</h5>";
 $_SESSION['tasks'] = $tasks;
 echo "<h5>4</h5>";
-header("location: home.php");
+header("location: home.php");*/
 
 ?>
