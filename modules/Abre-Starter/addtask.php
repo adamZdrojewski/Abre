db1080441ee3f53e8b398d57a14a98ab88cf3c72
BuildTools <unconfigured@null.spigotmc.org>
2019-03-25 21:40:43 -0400

    array test

--- modules/Abre-Starter/addtask.php
+++ modules/Abre-Starter/addtask.php
@@ -1,8 +1,12 @@
 <?php
 
+echo "<h5>1</h5>";
 $tasks = $_SESSION['tasks'];
+echo "<h5>2</h5>";
 array_push($tasks , $task);
+echo "<h5>3</h5>";
 $_SESSION['tasks'] = $tasks;
+echo "<h5>4</h5>";
 header("location: home.php");
 
 ?>
