6eeccf2dd0da945dd0d1b3bb06a4e20c17fbad60
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-04-01 17:27:51 +0000

    array test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -20,7 +20,7 @@
 	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
 	require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
     require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
-	session_start();
+	//session_start();
     $siteColor = getSiteColor();
 	if(!isset($_SESSION['tasks0']))
 	{
