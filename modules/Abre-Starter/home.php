bfada544cd8d5e75ac100eddda0e16ec0ef928fc
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-04-02 13:46:18 +0000

    array test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -42,10 +42,10 @@
             </div>
         </div>
         <div class='row'>
-            <form class='input-field col s10' method="post" id="planner" action="#starter/addtask">
+            <!--form class='input-field col s10' method="post" id="planner" action="#starter/addtask">
                 <input id="tta" type="text" class="validate" name="tasktoadd">
 				<button class="btn waves-effect waves-light" type="submit" name="action">Submit<i class="material-icons right">send</i></button>
-            </form>
+            </form-->
         </div>
 	</div>
 
