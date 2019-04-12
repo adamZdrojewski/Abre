0445a5a6367322f7cd119a914680554961995ad1
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-04-12 14:47:21 +0000

    array test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -37,7 +37,7 @@
             </div>
         </div>
         <div class='row'>
-            <form action="newaddtask.php" method="POST">
+            <form action="addtask.php" method="POST">
             <div class='input-field col s10'>
                 <input placeholder='newtask' id='newtask' type='text' name='newtask'>
             </div>
