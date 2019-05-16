930aea447cb02a547cf681cf9bb82e57555127db
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-05-16 10:38:33 -0400

    remove button test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -82,7 +82,7 @@
             foreach($tasklist as $currenttask)
             {
                 echo "<div class='row'>";
-                echo "<button class='btn-floating btn waves-effect waves-light col s1' style='background-color: ".$siteColor.";'><i class='material-icons'>remove</i></button>";
+                echo "<button class='btn-floating btn waves-effect waves-light' style='background-color: ".$siteColor.";'><i class='material-icons'>remove</i></button>";
                 echo "<p class='flow-text col s4 offset-s5'>".$currenttask."</p>";
                 echo "</div>";
             }
