90c6fc97204a3beaac938186e48cebb5a468fa7e
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-09-18 11:54:06 -0400

    new add task window

--- modules/Abre-Starter/newTaskForm.php
+++ modules/Abre-Starter/newTaskForm.php
@@ -79,3 +79,10 @@
         <br>
         
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+ </script>
