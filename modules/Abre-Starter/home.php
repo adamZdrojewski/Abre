f9aa03ea29fb6ecf81bfd92ee2ba77793978cd3d
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-09-18 10:53:21 -0400

    new add task window

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -144,8 +144,7 @@
 </div>
 
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
+$(document).ready(function(){
+    $('.modal').modal();
   });
 </script>
