e9bfe1fa58d735e5b6c874f5df9ce068dab01743
BuildTools <unconfigured@null.spigotmc.org>
2019-09-19 18:53:16 -0400

    new add task window

--- modules/Abre-Starter/modals.php
+++ modules/Abre-Starter/modals.php
@@ -84,7 +84,7 @@
 				</div>
 		
 		<div style='padding: 0px 24px 0px 24px;'>
-	    <!--div class="row center-align"-->
+	    <div class="row center-align">
 	      <p id="infoHolder" style=""></p>
 	    </div>
 		</div>
