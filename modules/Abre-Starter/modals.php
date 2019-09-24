fd27a999eda1f180cae34176c3e5551bf097a194
BuildTools <unconfigured@null.spigotmc.org>
2019-09-23 20:37:25 -0400

    new add task window

--- modules/Abre-Starter/modals.php
+++ modules/Abre-Starter/modals.php
@@ -66,7 +66,7 @@
 			
 					<div class='container'>
 						<div class='input-field'>
-							<input id="tasktoadd" name="tasktoadd" type="text" maxlength="200" placeholder="Name" autocomplete="off" required>
+							<input id="tasktoadd" name="tasktoadd" type="text" maxlength="200" placeholder="Add Task" autocomplete="off" required>
 						</div>
 						
 						
