d6a5ac4461b633d1f7bb6bec20652f4dea0b1a99
BuildTools <unconfigured@null.spigotmc.org>
2019-09-23 20:40:02 -0400

    new add task window

--- modules/Abre-Starter/modals.php
+++ modules/Abre-Starter/modals.php
@@ -125,5 +125,37 @@
 </div>
 
 <script>
-
+$(function(){
+    $('select').material_select();
+		$('.modal-newTaskModal').leanModal({ in_duration: 0, out_duration: 0, ready: function() { $('.modal-content').scrollTop(0); } });
+		$(document).on("click", ".modal-newTaskModal", function () {
+			var info = $(this).data('info');
+			$(".modal-content #infoHolder").text(info);
+		});
+	//you can use this code to send data to the server or another page if needed
+		// var formStarter = $('#form-starter');
+		//
+		// $(formStarter).submit(function(event) {
+		//   event.preventDefault();
+		//   var formData = $(formStarter).serialize();
+		//   $.ajax({
+		//     type: 'POST',
+		//     url: $(formStarter).attr('action'),
+		//     data: formData
+		//   })
+		//
+		//   //Show the notification
+		//   .done(function(responseprocess) {
+		//     //do something after the ajax call has sent data successfully
+		//   })
+		// });
+    var formStarter = $("#form-starter");
+    $(formStarter).submit(function(event){
+      event.preventDefault();
+      var results = $(formStarter).serialize();
+      results = results.replace(/&/g, ", ");
+      results = results.replace(/=/g, " = ");
+      $("#formResults").text(results);
+    });
+	});
 </script>
