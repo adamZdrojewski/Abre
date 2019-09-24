f5d7e7eb90390877bc75f81d4ecc397616260bf1
BuildTools <unconfigured@null.spigotmc.org>
2019-09-23 22:11:58 -0400

    category delete update

--- modules/Abre-Starter/categories.php
+++ modules/Abre-Starter/categories.php
@@ -65,7 +65,8 @@
 	<div class='page'>
 		<div class='row'>
 			<div style='padding:56px; text-align:center; width:100%;'>
-                <span style='font-size: 32px; font-weight:700'>Categories</span>
+                <span style='font-size: 32px; font-weight:700'>Categories</span><br>
+				<span style='font-size: 32px; font-weight:700'>Warning: Removing Categories Will Remove All Tasks In Them</span>
             </div>
         </div>
         <div class='row'>
