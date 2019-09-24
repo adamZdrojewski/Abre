7e7608aa4d806daf6ed15b8dcd7013d4148aae24
BuildTools <unconfigured@null.spigotmc.org>
2019-09-23 20:45:41 -0400

    new add task window

--- modules/Abre-Starter/modals.php
+++ modules/Abre-Starter/modals.php
@@ -37,10 +37,32 @@
 ?>
 
 <style>
-	/* label underline focus color */
+	/* label color */
+   .input-field label {
+     color: #000;
+   }
+   /* label focus color */
+   .input-field input[type=text]:focus + label {
+     color: #000;
+   }
+   /* label underline focus color */
    .input-field input[type=text]:focus {
-     border-bottom: 1px solid #000000;
-     box-shadow: 0 1px 0 0 #000000;
+     border-bottom: 1px solid #000;
+     box-shadow: 0 1px 0 0 #000;
+   }
+   /* valid color */
+   .input-field input[type=text].valid {
+     border-bottom: 1px solid #000;
+     box-shadow: 0 1px 0 0 #000;
+   }
+   /* invalid color */
+   .input-field input[type=text].invalid {
+     border-bottom: 1px solid #000;
+     box-shadow: 0 1px 0 0 #000;
+   }
+   /* icon prefix focus color */
+   .input-field .prefix.active {
+     color: #000;
    }
 </style>
 
