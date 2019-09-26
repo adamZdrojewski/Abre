6a01ea45f7a2dd27b4494fa3ec989b84a012dae6
BuildTools <unconfigured@null.spigotmc.org>
2019-09-26 15:19:29 -0400

    priority select

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -106,8 +106,8 @@
 					{
 					
 						echo "<tr>";
-						echo "<td style='font-size: 22px;'>{$currentname}</td>";
-						echo "<td><button class='btn {$currentpriority} col s1 right aligned offset-s1'></button></td>";
+						echo "<td style='font-size: 22px;' class='{$currentpriority}'>{$currentname}</td>";
+						echo "<td><button class='btn {$currentpriority} col s1 right aligned'></button></td>";
 						echo "<td>";
 						echo "<form class='' id='remove-task' method='post' action='modules/Abre-Starter/removetask.php'>";
 						echo "<input type='hidden' id='task' name='task' value='{$currentname}'>";
