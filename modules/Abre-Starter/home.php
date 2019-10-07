bcb7f25475d68273083e931d24d85da76f6b7670
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-10-07 08:09:33 -0400

    date picker

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -135,7 +135,9 @@
 <script>
 	$(function(){
     $('select').material_select();
-	$('.datepicker').datepicker();
+	$(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
 		$('.modal-newTaskModal').leanModal({ in_duration: 0, out_duration: 0, ready: function() { $('.modal-content').scrollTop(0); } });
 		$(document).on("click", ".modal-newTaskModal", function () {
 			var info = $(this).data('info');
