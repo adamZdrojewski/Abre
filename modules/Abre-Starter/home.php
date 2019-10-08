b3b3e5ca0d3a54b3ffcbca980ae216f8aea87ed7
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-10-08 07:25:02 -0400

    date picker test

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -135,14 +135,9 @@
 <script>
 	$(function(){
     $('select').material_select();
-	 $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-    container: undefined, // ex. 'body' will append picker to body
+	$(document).ready(function(){
+    $('.datepicker').datepicker();
+		container: 'body'
   });
 		$('.modal-newTaskModal').leanModal({ in_duration: 0, out_duration: 0, ready: function() { $('.modal-content').scrollTop(0); } });
 		$(document).on("click", ".modal-newTaskModal", function () {
