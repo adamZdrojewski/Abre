b19c3b787338e6214bd5a5a8842413d6f4348a41
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-10-15 10:31:28 -0400

    checking tasks

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -89,7 +89,7 @@
 				echo "<table id='myTable' class='tablesorter'>";
 				echo "<thead>";
 				echo "<th style='font-size: 30px;'>{$currentcategory}</th>";
-				echo "<th style='font-size: 30px;' class='right-align'>Priority</th>";
+				echo "<th style='font-size: 30px;' class=''>Priority</th>";
 				echo "<th style='font-size: 30px;' class='right-align'>Controls</th>";
 				echo "</thead>";
 				echo "<tbody>";
