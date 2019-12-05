b3d638a058cccdc40202049b3ee34b9b5ed58a61
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-12-05 10:52:14 -0500

    category home page layout update

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -105,8 +105,8 @@
 
             foreach($categorylist as $currentcategory)
 			{
-				echo "<table id='myTable' class='tablesorter'>";
 				echo "<h4 class='left align' style='font-size: 28px; font-weight: bold; padding-bottom: 5px; max-width:40%; -ms-word-break: break-all; word-break: break-all; margin:0;'>{$currentcategory}</h4>";
+				echo "<table id='myTable' class='tablesorter'>";
 				//echo "<caption class='left align' style='font-size: 28px; font-weight: bold; padding-bottom: 5px; max-width:40%; -ms-word-break: break-all; word-break: break-all;'>{$currentcategory}</caption>";
 				echo "<tbody>";
 
