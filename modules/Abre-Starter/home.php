2b5f0a9af87920e3177a45e391cf815d11c27cf9
adamZdrojewski <adam.zdrojewski2@gmail.com>
2019-12-06 10:26:20 -0500

    category home page layout update

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -107,7 +107,7 @@
 			{
 				//echo "<h4 class='left align' style='font-size: 28px; font-weight: bold; padding-bottom: 5px; max-width:40%; -ms-word-break: break-all; word-break: break-all; margin:0;'>{$currentcategory}</h4>";
 				echo "<table id='myTable' class='tablesorter' style='margin-top:0;'>";
-				echo "<caption class='left align' style='font-size: 28px; font-weight: bold; padding-bottom: 5px; max-width:40%; -ms-word-break: break-all; word-break: break-all;'>{$currentcategory}</caption>";
+				echo "<caption class='left align' style='font-size: 28px; font-weight: bold; padding-bottom: 5px; max-width:100%; -ms-word-break: break-all; word-break: break-all;'>{$currentcategory}</caption>";
 				echo "<tbody>";
 
 				foreach($tasklist as $currenttask)
