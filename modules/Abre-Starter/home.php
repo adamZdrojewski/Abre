4e1414b505dcb951e92982b0c9fbdd0049ede3d7
BuildTools <unconfigured@null.spigotmc.org>
2019-12-08 13:29:23 -0500

    category home page layout update

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -105,7 +105,7 @@
 
             foreach($categorylist as $currentcategory)
 			{
-				echo "<h3 class='left align' style='margin:0; font-size: 28px; font-weight: bold; padding-bottom: 5px; max-width:40%; -ms-word-break: break-all; word-break: break-all; background-color:green;'>{$currentcategory}</h3>";
+				echo "<h3 class='left align' style='margin:0; font-size: 28px; font-weight: bold; padding-bottom: 5px; max-width:40%; -ms-word-break: break-all; word-break: break-all;'>{$currentcategory}</h3>";
 				echo "<table id='myTable' class='tablesorter' style='margin-top:0;'>";
 				//echo "<caption class='left align' style='font-size: 28px; font-weight: bold; padding-bottom: 5px; max-width:40%; -ms-word-break: break-all; word-break: break-all; background-color:green;'>{$currentcategory}</caption>";
 				echo "<tbody>";
