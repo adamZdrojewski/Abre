8ca829b243b9f5f2b7fe0012e54c426cbb97729a
adamZdrojewski <adam.zdrojewski2@gmail.com>
2020-02-06 12:03:12 -0500

    mobile design

--- modules/Abre-Starter/categories.php
+++ modules/Abre-Starter/categories.php
@@ -114,7 +114,7 @@
                 echo "<input type='hidden' id='category' name='category' value='".$currentcategory."'>";
                 echo "<button class='btn waves-effect waves-light col s0.75' style='background-color: ".$siteColor.";'><i class='material-icons'>remove</i></button>";
                 echo "</form>";
-                echo "<p class='flow-text col s10 offset-s1' style='-ms-word-break: break-all; word-break: break-all; width:78%; max-width:78%;'>".$currentcategory."</p>";
+                echo "<p class='flow-text col s10 offset-s1' style='-ms-word-break: break-all; word-break: break-all; width:79%; max-width:79%;'>".$currentcategory."</p>";
                 echo "</div>";
                 echo "</div>";
             }
